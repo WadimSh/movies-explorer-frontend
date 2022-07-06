b98b2fb01bb6d98801aff07a515bfa61c030d0af
WadimSh <wadim11@yandex.ru>
2022-07-06 11:38:53 +0300

    feat: собран компонент seved-movies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,9 +1,16 @@
 import React from 'react';
+
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+
 import './SavedMovies.css';
 
 function SavedMovies() {
   return (
-    <h1>SavedMovies</h1>
+    <main className="saved-movies">
+    <SearchForm />
+    <MoviesCardList />
+    </main>
   );
 }
 
