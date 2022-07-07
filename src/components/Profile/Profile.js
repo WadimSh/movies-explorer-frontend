8490fbcc6bb2fd84b0a40086cb8d958dc7b80984
WadimSh <wadim11@yandex.ru>
2022-07-07 20:52:28 +0300

    fix: правки компонента profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ import './Profile.css';
 function Profile() {
   return (
     <section className="profile">
-      <form className="profile-form" name="login" action="#" noValidate>
+      <form className="profile-form">
         <h1 className="profile-form__title">Привет, Вадим!</h1>
 
           <label className="profile-form__label">
@@ -17,8 +17,8 @@ function Profile() {
           <input id="email-input" type="text" name="email" placeholder="E-mail" className="profile-form__input profile-form__input_type_email" required/>
         </label>
 
-        <button type="submit" className="profile-form__submit">Редактировать</button>
-        <button className="profile__logout">Выйти из аккаунта</button>
+        <button className="profile-form__submit">Редактировать</button>
+        <button className="profile__logout" type="submit">Выйти из аккаунта</button>
       </form>
     </section>
   );
