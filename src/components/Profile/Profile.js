c4177e77c29ba2ad7c63ac8f63ca78bd22ea2db5
WadimSh <wadim11@yandex.ru>
2022-07-07 20:43:16 +0300

    feat: сформирован компонент profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,7 +3,24 @@ import './Profile.css';
 
 function Profile() {
   return (
-    <h1>Profile</h1>
+    <section className="profile">
+      <form className="profile-form" name="login" action="#" noValidate>
+        <h1 className="profile-form__title">Привет, Вадим!</h1>
+
+          <label className="profile-form__label">
+          <span className="profile-form__label-text">Имя</span>
+            <input id="name-input" type="text" name="name" placeholder="Имя" className="profile-form__input profile-form__input_type_name" minLength="2" maxLength="30" required/>
+          </label>
+
+        <label className="profile-form__label">
+          <span className="profile-form__label-text">E-mail</span>
+          <input id="email-input" type="text" name="email" placeholder="E-mail" className="profile-form__input profile-form__input_type_email" required/>
+        </label>
+
+        <button type="submit" className="profile-form__submit">Редактировать</button>
+        <button className="profile__logout">Выйти из аккаунта</button>
+      </form>
+    </section>
   );
 }
 
