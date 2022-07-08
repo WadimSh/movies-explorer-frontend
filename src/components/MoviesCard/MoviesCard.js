f2ac0839d7259d7ca0b93d85f981ab1d223c2945
WadimSh <wadim11@yandex.ru>
2022-07-08 13:15:10 +0300

    fix: отформатирован компонент movies-card

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -4,11 +4,10 @@ import { Route, Switch } from 'react-router-dom';
 import './MoviesCard.css';
 
 function MoviesCard({ movieCard, onCardSaved, onCardDelete, onCheck }) {
-
   const [isMoviesSeved, setMoviesSeved] = React.useState(movieCard.check);
   const [isCardDelete, setCardDelete] = React.useState(false);
   
-  function handleMoviesSeved() {
+  const handleMoviesSeved = () => {
     if (!isMoviesSeved) {
       setMoviesSeved(true);
       onCardSaved(movieCard);
@@ -16,35 +15,34 @@ function MoviesCard({ movieCard, onCardSaved, onCardDelete, onCheck }) {
       setMoviesSeved(false);
       onCardDelete(movieCard);
     }
-    
   }
 
-  function handleMoviesDelete() {
+  const handleMoviesDelete = () => {
     onCardDelete(movieCard);
     setCardDelete(true);
   }
 
-return (
-  <li className={!isCardDelete ? `movies-card` : `movies-card__delete`} key={movieCard.id}>
-    <div className="movies-card__header">
-      <div className="movies-card__info">
-        <h2 className="movies-card__title">{movieCard.title}</h2>
-        <span className="movies-card__duration">{movieCard.duration}</span>
+  return (
+    <li className={!isCardDelete ? `movies-card` : `movies-card__delete`} key={movieCard.id}>
+      <div className="movies-card__header">
+        <div className="movies-card__info">
+          <h2 className="movies-card__title">{movieCard.title}</h2>
+          <span className="movies-card__duration">{movieCard.duration}</span>
+        </div>
+        <Switch>
+          <Route path="/movies">
+            <button className={!isMoviesSeved ? `movies-card__button movies-card__button_type_save` : `movies-card__button movies-card__button_type_save-active`} onClick={handleMoviesSeved} type="button"></button>
+          </Route>
+          <Route path="/saved-movies">
+            <button className="movies-card__button movies-card__button_type_delete" onClick={handleMoviesDelete} type="button"></button>
+          </Route>
+        </Switch>
       </div>
-      <Switch>
-        <Route path="/movies">
-          <button className={!isMoviesSeved ? `movies-card__button movies-card__button_type_save` : `movies-card__button movies-card__button_type_save-active`} onClick={handleMoviesSeved} type="button"></button>
-        </Route>
-        <Route path="/saved-movies">
-        <button className="movies-card__button movies-card__button_type_delete" onClick={handleMoviesDelete} type="button"></button>
-        </Route>
-      </Switch>
-    </div>
-    <a className="movies-card__link" href="#">
-      <img className="movies-card__cover" src={movieCard.image} alt="Обложка фильма" />
-    </a>
-  </li>
-);
+      <a className="movies-card__link" href="#">
+        <img className="movies-card__cover" src={movieCard.image} alt="Обложка фильма" />
+      </a>
+    </li>
+  )
 }
 
 export default MoviesCard;
