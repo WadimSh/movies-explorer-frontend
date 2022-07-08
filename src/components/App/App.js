5f663451ab2f4da207e79a0f4c3b788742005b83
WadimSh <wadim11@yandex.ru>
2022-07-08 11:49:00 +0300

    fix: отформатирован компонент app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,89 +16,77 @@ import cardsList from '../../utils/cardsList';
 import './App.css';
 
 function App() {
-
   const [isLoggedIn, setLoggedIn] = React.useState(true);
   const history = useHistory();
   const saveCardsList = [];
-
-  function handleSignOut() {
+  
+  //функция выхода из аккауна
+  const handleSignOut = () => {
     setLoggedIn(true);
     history.push('/');
   }
 
-  function handleSeveMovies(movieCard) {
-    const seve = saveCardsList.includes(movieCard)
-    if (!seve) {
-        movieCard.check = true;
-        saveCardsList.push(movieCard);
-      }
+  //функция сохранения карточки фильма
+  const handleSeveMovies = (movieCard) => {
+    const seach = saveCardsList.includes(movieCard)
+    if (!seach) {
+      movieCard.check = true;
+      saveCardsList.push(movieCard);
     }
-  
-
-  function handleDeleteMovies(movieCard) {
-    const seve = saveCardsList.includes(movieCard)
-    if (seve) {
-        movieCard.check = false;
-        const del = saveCardsList.indexOf(movieCard);
-        saveCardsList.splice(del, 1);
-      }
   }
 
+  //функция удаления карточки фильма
+  const handleDeleteMovies = (movieCard) => {
+    const seach = saveCardsList.includes(movieCard)
+    if (seach) {
+      movieCard.check = false;
+      const del = saveCardsList.indexOf(movieCard);
+      saveCardsList.splice(del, 1);
+    }
+  }
 
   return (
     <div className="page">
       <Header 
         loggedIn={isLoggedIn}
       />
-    
       <Switch>
-        
         <Route exact path="/">
           <Main />
         </Route>
-        
         <Route path="/movies">
           <Movies 
-           cardsList={cardsList}
-           onCardSaved={handleSeveMovies}
-           onCardDelete={handleDeleteMovies}
-           onMoreButton={true}
+            cardsList={cardsList}
+            onCardSaved={handleSeveMovies}
+            onCardDelete={handleDeleteMovies}
+            onMoreButton={true}
           />
         </Route>
-          
         <Route path="/saved-movies">
           <SavedMovies
-          cardsList={saveCardsList}
-          onCardSaved={handleSeveMovies}
-          onCardDelete={handleDeleteMovies}
-          onMoreButton={false}
+            cardsList={saveCardsList}
+            onCardSaved={handleSeveMovies}
+            onCardDelete={handleDeleteMovies}
+            onMoreButton={false}
           />
         </Route>
-          
         <Route path="/profile">
           <Profile
-          onSignOut={handleSignOut}
+            onSignOut={handleSignOut}
           />
         </Route>
-          
         <Route path="/signin">
-          <Login
-           
-          />
+          <Login />
         </Route>
-          
         <Route path="/signup">
           <Register
-          setLoggedIn={setLoggedIn}
+            setLoggedIn={setLoggedIn}
           />
         </Route>
-          
         <Route path="*">
           <PageNotFound />
         </Route>
-        
-        </Switch>
-        
+      </Switch>
       <Footer />
     </div>
   );
