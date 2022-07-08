2c28f478aba61dbf97f4d829060b2b40e46243c4
WadimSh <wadim11@yandex.ru>
2022-07-08 11:59:52 +0300

    fix: отформатирован компонент header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 
 import Navigation from '../Navigation/Navigation';
+
 import Logo from '../../images/logo.svg';
 import './Header.css';
 
 function Header({ loggedIn }) {
+
   return (
     <Route exact path="/(|movies|saved-movies|profile)">
         <header className="header">
-        <div className="header__wrap">
-          <Link to="/">
-            <img src={Logo} alt="Логотип сайта" />
-          </Link>
-          <Navigation loggedIn={loggedIn} />
-        </div>
+          <div className="header__wrap">
+            <Link to="/">
+              <img src={Logo} alt="Логотип сайта" />
+            </Link>
+            <Navigation loggedIn={loggedIn} />
+          </div>
         </header>
       </Route>
  );
