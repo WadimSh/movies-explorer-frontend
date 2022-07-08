70b8be7bbd3ad14d627f41a2ac37609fa21d8f0b
WadimSh <wadim11@yandex.ru>
2022-07-08 13:56:23 +0300

    fix: отформатирован компонент navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,34 +4,32 @@ import { Link, NavLink } from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation({ loggedIn }) {
-
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  function handleMenuOpen() {
+  const handleMenuOpen = () => {
     setIsMenuOpen(true);
   }
 
-  function handleMenuClose() {
+  const handleMenuClose = () => {
     setIsMenuOpen(false);
   }
 
   return (
-    <nav className='navigation'>
+    <nav className="navigation">
       {loggedIn ? (
           <>
-            <Link className='navigation__user-register' to="/signup"> Регистрация </Link>
-            <Link className='navigation__user-login' to="/signin"> Войти </Link>
+            <Link className="navigation__user-register" to="/signup">Регистрация</Link>
+            <Link className="navigation__user-login" to="/signin">Войти</Link>
           </>
         ) : (
-          <div className='navigation__destop-menu'>
-            <NavLink className='navigation__site-link' activeClassName="navigation__site-link_active" to="/movies"> Фильмы </NavLink>
-            <NavLink className='navigation__site-link' activeClassName="navigation__site-link_active" to="/saved-movies"> Сохранённые фильмы </NavLink>
-            <Link className='navigation__user-profile' to="/profile"> Аккаунт </Link>
-            <button className='navigation__button' onClick={handleMenuOpen} type="button"></button>
+          <div className="navigation__destop-menu">
+            <NavLink className="navigation__site-link" activeClassName="navigation__site-link_active" to="/movies">Фильмы</NavLink>
+            <NavLink className="navigation__site-link" activeClassName="navigation__site-link_active" to="/saved-movies">Сохранённые фильмы</NavLink>
+            <Link className="navigation__user-profile" to="/profile">Аккаунт</Link>
+            <button className="navigation__button" onClick={handleMenuOpen} type="button"></button>
           </div>
         )
       }
-
       <div className={`navigation__mobile-menu mobile-menu ${isMenuOpen ? "mobile-menu_is-open" : ""}`}>
         <div className="mobile-menu__links">
           <button className="mobile-menu__close" onClick={handleMenuClose} type="button"></button>
@@ -41,10 +39,8 @@ function Navigation({ loggedIn }) {
           <Link className="mobile-menu__user-profile" to="/profile" onClick={handleMenuClose}>Аккаунт</Link>
         </div>
       </div>
-
     </nav>
-    
-  );
+  )
 }
 
 export default Navigation;
