bae50b8113b2815a17d1ad7bd8bbb1aea12c4cb4
WadimSh <wadim11@yandex.ru>
2022-07-08 13:29:46 +0300

    fix: отформатирован компонент movies-card-list

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+
 import MoviesCard from '../MoviesCard/MoviesCard';
 
 import './MoviesCardList.css';
@@ -7,23 +7,20 @@ import './MoviesCardList.css';
 function MoviesCardList({ cardsList, onCardSaved, onCardDelete, onMoreButton }) {
   
   return (
-     <section className="movies-card-list-container">
-      <ul className="movies-card-list">
-            {cardsList.map((movieCard) => (
-              <MoviesCard
-                key={movieCard.id}
-                movieCard={movieCard}
-                onCardSaved={onCardSaved}
-                onCardDelete={onCardDelete}
-                
-              />
-            ))}
-               
+    <section className="movies-card-list">
+      <ul className="movies-card-list__container">
+        {cardsList.map((movieCard) => (
+          <MoviesCard
+            key={movieCard.id}
+            movieCard={movieCard}
+            onCardSaved={onCardSaved}
+            onCardDelete={onCardDelete}
+          />
+        ))}
       </ul>
-      <div className='movies-card-list__box'>
+      <div className="movies-card-list__box">
         <button className={onMoreButton ? `movies-card-list__more-button` : `movies-card-list__more-button movies-card-list__more-button_unactive`} type="button">Ещё</button>
       </div>
-            
     </section>
   )
 }
