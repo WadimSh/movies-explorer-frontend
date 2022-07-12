ed89fc761103e54af1ddd4ec0d782481f72efbf6
WadimSh <wadim11@yandex.ru>
2022-07-12 13:21:27 +0300

    feat: сформирована логика компонента saved-movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -5,24 +5,35 @@ import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Preloader from '../Preloader/Preloader';
 
 import moviesApi from '../../utils/MoviesApi';
-import moviesFilter from '../../utils/MoviesFilter'; 
+import moviesFilter from '../../utils/MoviesFilter';
 
 import './Movies.css';
 
-function Movies({ savedMoviesByCurrentUser, onCardSaved, onCardDelete }) {
-  const [moviesToRender, setMoviesToRender] = React.useState([]);
+function Movies({ cardsList, onCardSaved, onCardDelete }) {
+  //переменная в которой записан текст запроса из поисковой стороки
   const [query, setQuery] = React.useState('');
-  const [checkboxStatus, setCheckboxStatus] = React.useState(false);
+  //массив с отображаемым колличеством карточек
+  const [moviesToRender, setMoviesToRender] = React.useState([]);
+  //массив с исходным колличеством фильмов
   const [initialMovies, setInitialMovies] = React.useState([]);
-  const [isSearchMovies, setSearchMovies] = React.useState(false);
+  //массив с результатами поиска
   const [filteredMovies, setFilteredMovies] = React.useState([]);
+  //логическая переменная для определения состояния загрузки массива фильма, т.е. когда подключать спинер
+  const [isSearchMovies, setSearchMovies] = React.useState(false);
+  //логическая переменная состояния чекбокса - фильтра короткометражек
+  const [checkboxStatus, setCheckboxStatus] = React.useState(false);
+  //логическая переменная состояния на старте, т.е. до поиска
   const [isSearchDone, setIsSearchDone] = React.useState(false);
+  //логическая переменная отображения кнопки ещё
   const [isMoreButtonVisible, setIsMoreButtonVisible] = React.useState(false);
+  //переменные для отображения колличества карточек в зависимости от расширения экрана
+  //колличество первого паказа карточек
   const [firstResultsNumber, setFirstResultsNumber] = React.useState(0);
+  //колличество последующего показа карточек
   const [moreResultsNumber, setMoreResultsNumber] = React.useState(0);
+  //переменная в которую записана текущая ширина экрана
   const currentViewport = document.documentElement.clientWidth;
-
-
+  //основная функция передаваемая для запуска в форму поиска
   const handleSearch = (query, checkboxStatus) => {
     setMoviesToRender([]);
     setQuery(query);
@@ -47,17 +58,15 @@ function Movies({ savedMoviesByCurrentUser, onCardSaved, onCardDelete }) {
       setInitialMovies(initialMoviesInLocalStorage);
     }
   }
-
+  //эффект который осуществляет поиск и фильтрацию из исходного массива
   React.useEffect(() => {
     if (initialMovies.length > 0) {
       const searchResults = moviesFilter(initialMovies, query, checkboxStatus);
       setFilteredMovies(searchResults);
-      setIsSearchDone(true);
-      console.log(searchResults);
+      setIsSearchDone(true);      
     }
-    
   }, [initialMovies, query, checkboxStatus]);
-
+  //эффект определения отображения карточек взависимости от ширены экрана
   React.useEffect(() => {
     if (currentViewport <= 480) {
       setFirstResultsNumber(5);
@@ -70,22 +79,26 @@ function Movies({ savedMoviesByCurrentUser, onCardSaved, onCardDelete }) {
       setMoreResultsNumber(3);
     }
   }, [currentViewport]);
-
+  //эффект выбора колличества карточек показа
   React.useEffect(() => {
     if (filteredMovies.length > 0) {
       if (filteredMovies.length > firstResultsNumber) {
+        
         setMoviesToRender(filteredMovies.slice(0, firstResultsNumber));
         setIsMoreButtonVisible(true);
       } else {
         setMoviesToRender(filteredMovies);
+        
       }
+      
     }
   }, [filteredMovies, firstResultsNumber]);
-
+  //функция работы кнопки ещё
   function handleMoreButtonClick() {
     setMoviesToRender((state) => filteredMovies.slice(0, state.length + moreResultsNumber));
+    
   }
-
+  //проверка видемости кнопки ещё
   React.useEffect(() => {
     if (moviesToRender.length === filteredMovies.length) {
       setIsMoreButtonVisible(false);
@@ -103,7 +116,7 @@ function Movies({ savedMoviesByCurrentUser, onCardSaved, onCardDelete }) {
           ? moviesToRender.length > 0
             ? <MoviesCardList 
                 movies={moviesToRender}
-                savedMoviesByCurrentUser={savedMoviesByCurrentUser}
+                cardsList={cardsList}
                 onCardSaved={onCardSaved}
                 onCardDelete={onCardDelete}
                 isMoreButtonVisible={isMoreButtonVisible}
