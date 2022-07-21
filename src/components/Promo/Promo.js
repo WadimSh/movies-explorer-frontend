be32ba0b4f92c49081b21cd27563b3335b54462c
WadimSh <wadim11@yandex.ru>
2022-07-21 11:48:01 +0300

    fix: добавлена анимация по скролу

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -3,11 +3,21 @@ import React from 'react';
 import './Promo.css';
 
 function Promo(props) {
+  const [scroll, setScroll] = React.useState(0);
+
+  const handleScroll = () => {
+    setScroll(window.scrollY);
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [])
   
   return (
     <section className="promo">
       <div className="promo__container">
-        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+        <h1 className={`promo__title ${scroll < 300 ? "promo__title-active" : ""}`}>Учебный проект студента факультета Веб-разработки.</h1>
         {props.children}
       </div>
     </section>
