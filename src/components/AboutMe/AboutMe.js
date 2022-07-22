b31008498e9bb2cffc38b4005d2ae993153e1345
WadimSh <wadim11@yandex.ru>
2022-07-22 12:21:33 +0300

    fix: доработана анимация стартовой страницы

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,16 +3,16 @@ import React from 'react';
 import Photo from '../../images/about-me.jpg';
 import './AboutMe.css';
 
-function AboutMe() {
+function AboutMe(props) {
 
   return (
     <section className="about-me" id="about-me">
-      <h2 className="about-me__title">Студент</h2>
+      <h2 className={`about-me__title ${props.onScroll < 1400 ? "" : "about-me__title-active"}`}>Студент</h2>
       <div className="about-me__wrap">
         <div className="about-me__info">
-          <h3 className="about-me__name">Вадим</h3>
-          <p className="about-me__profile">Фронтенд-разработчик, 43 года</p>
-          <p className="about-me__description">Я&nbsp;живу в&nbsp;Москве, закончил факультет АСУ ВМИРЭ. У&nbsp;меня есть жена и&nbsp;дочь. Я&nbsp;люблю слушать музыку. Недавно решил освоить профессию веб-разработчика. Пока есть идеи как применить новые знания на&nbsp;текущей работе, а&nbsp;будущем возможно полностью перейду в&nbsp;программисты.</p>
+          <h3 className={`about-me__name ${props.onScroll < 1500 ? "" : "about-me__name-active"}`}>Вадим</h3>
+          <p className={`about-me__profile ${props.onScroll < 1600 ? "" : "about-me__profile-active"}`}>Фронтенд-разработчик, 43 года</p>
+          <p className={`about-me__description ${props.onScroll < 1700 ? "" : "about-me__description-active"}`}>Я&nbsp;живу в&nbsp;Москве, закончил факультет АСУ ВМИРЭ. У&nbsp;меня есть жена и&nbsp;дочь. Я&nbsp;люблю слушать музыку. Недавно решил освоить профессию веб-разработчика. Пока есть идеи как применить новые знания на&nbsp;текущей работе, а&nbsp;будущем возможно полностью перейду в&nbsp;программисты.</p>
           <ul className="about-me__links-list">
             <li>
               <a className="about-me__links-item" href="https://www.facebook.com" target="_blank">Facebook</a>
--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -6,10 +6,10 @@ function Techs(props) {
 
   return (
     <section className="techs" id="techs">
-      <div className={`techs__container ${props.onScroll < 800 ? "" : "techs__container-active"}`}>
-        <h2 className="techs__title" >Технологии</h2>
-        <h3 className="techs__subtitle">7 технологий</h3>
-        <p className="techs__paragraph">На&nbsp;курсе веб-разработки мы&nbsp;освоили технологии, которые применили в&nbsp;дипломном проекте.</p>
+      <div className="techs__container">
+        <h2 className={`techs__title ${props.onScroll < 800 ? "" : "techs__title-active"}`}>Технологии</h2>
+        <h3 className={`techs__subtitle ${props.onScroll < 900 ? "" : "techs__subtitle-active"}`}>7 технологий</h3>
+        <p className={`techs__paragraph ${props.onScroll < 1000 ? "" : "techs__paragraph-active"}`}>На&nbsp;курсе веб-разработки мы&nbsp;освоили технологии, которые применили в&nbsp;дипломном проекте.</p>
         <ul className="techs__list">
           <li className="techs__list-item">HTML</li>
           <li className="techs__list-item">CSS</li>
